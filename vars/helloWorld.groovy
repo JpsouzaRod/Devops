e0a2fd71bf90c1af3a530971baf1db1b53df7c9f
João Paulo Rodrigues <jpsouza.rod@gmail.com>
2024-10-03 12:28:35 -0300

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -6,26 +6,12 @@ def call()
 		stages
 		{
 
-			stage('init')
-			{
-				steps
-				{
-					script 
-					{
-						deleteDir()
-						sh 'git clone https://github.com/JpsouzaRod/challenge-back-end-hit.git'
-
-                			}
-
-				}
-			}
 			stage('build')
 			{
 				steps
 				{
 					script 
 					{
-						sh 'cd ${WORKSPACE}/PipelineGroovyTest_master/challenge-back-end-hit/StarWarsChallenge'
                     				sh 'dotnet build StarWarsChallenge.sln'
                 			}
 
